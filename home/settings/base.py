cbe696b0d41a3752d089b94e450232c6fee1ddd4
kaylinkhanal <kyalin.khanal@gmail.com>
2019-05-11 15:49:24 +0545

    userid assign issue solved

--- home/settings/base.py
+++ home/settings/base.py
@@ -74,9 +74,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ),
 }
 
 CSRF_COOKIE_NAME = "csrftoken"
--- home/settings/dev.py
+++ home/settings/dev.py
@@ -10,12 +10,8 @@ WSGI_APPLICATION = 'home.wsgi.dev.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Nalytica',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
